efc1d1c6944be2559d5b9eeb9ca13d05424c8dbf
Chris <kurisukodo@gmail.com>
2023-10-13 10:55:32 +0800

    main.py, file formats, and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python-filter-organizer
+
+Python filter organizer is a simple python script that helps to organize any files in a folder based on file formats specified in a json file.
+
+## Installation
+
+Install python3 for your system and run the following command to execute the script
+
+```base
+python3 main.py \path-to-your-folder-to-organize
+```
---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+import os
+import shutil
+import sys
+
+def clean_folder(file_formats: dict, target_directory: str):
+    print(f"Starting to clean '{target_directory}' folder")
+
+    dir_content = os.listdir(target_directory)
+
+    for d_file in dir_content:
+        try:
+            if os.path.isdir(d_file):
+                continue
+
+            file_name, file_extension = os.path.splitext(d_file)
+            if not file_extension.lower() in file_formats:
+                continue
+
+            target_path = f'{target_directory}/{file_formats[file_extension.lower()]}'
+            if not os.path.isdir(target_path):
+                os.mkdir(target_path)
+
+            src_path = f'{target_directory}/{d_file}'
+            dst_path = f'{target_path}/{d_file}'
+
+            shutil.copyfile(src = src_path, dst = dst_path)
+
+            os.remove(src_path)
+
+        except:
+            print(f"Failed to move file {d_file}")
+
+    print(f"Finished cleaned '{target_directory}' folder")
+
+if __name__ == '__main__':
+
+    args = sys.argv
+
+    if len(args) < 2:
+        raise Exception("Target folder must be passed to the command")
+
+    target_directory = args[1]
+
+    file_formats = dict()
+
+    with open("file_formats.json", 'r') as file:
+        file_formats = json.load(file)
+
+    if len(file_formats) == 0:
+        print("Failed to execute because of missing file formats")
+    else:
+        clean_folder(file_formats = file_formats, target_directory = target_directory)
